f795f495fbbf96b98b6423582845e80d4783d7b1
Morne Zeelie <morne.zeelie@clickatell.com>
2017-10-19 13:53:14 +0200

    made fixes to be able to send to more than one number for REST

--- api/rest.js
+++ api/rest.js
@@ -11,6 +11,8 @@ function Rest(content, to, apiKey) {
 // class methods
 Rest.prototype.sendMessage = function(content, to, apiKey) {
     // set options array
+    content = JSON.stringify(content);
+    to = JSON.stringify(to);
     var options = { 
       method: 'POST',
       url:'https://platform.clickatell.com/messages', 
@@ -20,14 +22,14 @@ Rest.prototype.sendMessage = function(content, to, apiKey) {
         'Accept': 'application/json',
         'Authorization': apiKey
       },
-      form: {'content': content, 'to': to.join(",")}
+      body: '{"content": ' + content + ', "to": ' + to +'}'
     }
+
     // send the request
     Request(options, function (error, response, body) {
-
-    console.log('error:', error); // Print the error if one occurred
-    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    console.log('body:', body); // Print the HTML for the Google homepage.
+      console.log('error:', error); // Print the error if one occurred
+      console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+      console.log('body:', body); // Print the HTML for the Google homepage.
     }); 
 };
 // export the class
